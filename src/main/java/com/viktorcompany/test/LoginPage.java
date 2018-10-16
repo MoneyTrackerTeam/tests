83903b9921b70fb53160203c39bf74194b6c2467
Viktor <v.melnychuk12@gmail.com>
2018-10-16 23:34:44 +0300

    some tests added

--- src/main/java/com/viktorcompany/test/LoginPage.java
+++ src/main/java/com/viktorcompany/test/LoginPage.java
@@ -1,44 +1,49 @@
 package com.viktorcompany.test;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class LoginPage {
-    private final SelenideElement usernameInp;
-    private final SelenideElement passwrodInp;
-    private final SelenideElement loginBtn;
+    public final SelenideElement usernameInp;
+    public final SelenideElement passwordInp;
+    public final SelenideElement loginBtn;
 
     public LoginPage(){
         usernameInp = $("#mat-input-0");
-        passwrodInp = $("#mat-input-1");
-        loginBtn = $(By.cssSelector("body > app-root > div > mat-sidenav-container > mat-sidenav-content > app-login > mat-card > mat-card-actions > button"));
+        passwordInp = $("#mat-input-1");
+        loginBtn = $("#login");
     }
 
     @Step("Open {url}")
-    public LoginPage openLoginPage(String url){
-        open(url);
+    public LoginPage openLoginPage(){
+        open("http://localhost:4200/login");
         return  this;
     }
 
-    @Step("Fill username")
+    @Step("Fill username field with '{value}'")
     public LoginPage fillUsername (String value){
         usernameInp.setValue(value);
         return  this;
     }
-    @Step("Fill password")
+    @Step("Fill password field with '{value}'")
     public LoginPage fillPassword (String value){
-        passwrodInp.setValue(value);
-        return  this;
+        passwordInp.setValue(value);
+        return this;
     }
 
     @Step("Click Login button")
     public LoginPage clickLoginButton (){
         loginBtn.click();
+        return this;
+    }
+
+    @Step("Clear Username and Password fields")
+    public LoginPage clearUsernameAndPassword(){
+        usernameInp.clear();
+        passwordInp.clear();
         return this;
     }
 }
--- src/test/java/com/viktorcompany/test/LoginPageTest.java
+++ src/test/java/com/viktorcompany/test/LoginPageTest.java
@@ -1,17 +1,41 @@
 package com.viktorcompany.test;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
+
 public class LoginPageTest {
-    @Test
+    @Test(description = "User can login with valid credentials")
+    @Severity(SeverityLevel.BLOCKER)
     public void checkLoginWithValidCredentials(){
-        new LoginPage().openLoginPage("http://localhost:4200/login")
+        new LoginPage().openLoginPage()
                 .fillUsername("1")
                 .fillPassword("1")
                 .clickLoginButton();
         new TransactionsPage().checkHeaderDisplayed();
+    }
+
+    @Test(description = "User cannot login with invalid credentials")
+    @Severity(SeverityLevel.BLOCKER)
+    public void checkUserNotLoggedInWithInvalidCredentials(){
+        LoginPage page = new LoginPage().openLoginPage()
+                .fillUsername("1")
+                .fillPassword("2")
+                .clickLoginButton();
+        assert page.usernameInp.isDisplayed();
+    }
+
+    @Test(description = "Username and Password fields are required")
+    @Severity(SeverityLevel.CRITICAL)
+    public void usernameAndPasswordFieldsAreRequired(){
+        LoginPage page = new LoginPage().openLoginPage();
+        page.fillUsername("1").clickLoginButton();
+        assert page.usernameInp.isDisplayed();
+        assert page.passwordInp.has(Condition.attribute("aria-invalid"));
     }
 }
